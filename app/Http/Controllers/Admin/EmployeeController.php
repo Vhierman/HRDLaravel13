222c12279f77ff948a0c76862435c0809bf9310c
Vhierman <achmadfirmansyah989@yahoo.co.id>
2026-05-13 08:11:04 +0700

    Refactor: Rapihkan Token Controller EMployee

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -49,7 +49,6 @@ class EmployeeController extends Controller
                     'divisions',
                     'positions'
                     ])->get();
-
         return view('admin.pages.employee.index',[
             'employees' => $employees
         ]);
@@ -71,7 +70,6 @@ class EmployeeController extends Controller
         $divisions      = Divisions::all();
         $positions      = Positions::all();
         $areas          = Areas::all();
-
         return view ('admin.pages.employee.create',[
             'companies'     => $companies,
             'working_hours' => $working_hours,
@@ -98,11 +96,9 @@ class EmployeeController extends Controller
         //Cari Minimal Upah Dari Area
         
         $data       = $request->except(['_token', '_method']);
-
         $fileFields = [
             'foto_karyawan' => 'assets/foto/karyawan'
         ];
-
         foreach ($fileFields as $field => $path) {
             if ($request->hasFile($field)) {
                 // Ambil objek filenya, bukan status boolean-nya
@@ -123,7 +119,6 @@ class EmployeeController extends Controller
         }
 
         Employees::create($data);
-        
         Alert::success('Success Input Data Karyawan','Oleh '.auth()->user()->name);
         return redirect()->route('employee.index');
 
@@ -146,18 +141,14 @@ class EmployeeController extends Controller
         $positions              = Positions::all();
         $working_hours          = WorkingHours::all();
         $areas                  = Areas::all();
-
         $today                  = Carbon::today();
         $tanggal_lahir          = Carbon::parse($employee->tanggal_lahir);
         $tanggal_mulai_kerja    = Carbon::parse($employee->tanggal_mulai_kerja);
-
         $UmurLengkap            = $tanggal_lahir->diff($today)->format('%y Tahun, %m Bulan');
         $MasaKerja              = $tanggal_mulai_kerja->diff($today)->format('%y Tahun, %m Bulan');
-
         $history_contracts      = HistoryContracts::with(['employees'])->get();
         $history_families       = HistoryFamilies::with(['employees'])->get();
         $history_family         = HistoryFamilies::with(['employees'])->where('employees_id', $employee->id)->first();
-
         $history_positions      = HistoryPositions::with([
                                     'employees',
                                     'divisions',
@@ -165,7 +156,6 @@ class EmployeeController extends Controller
                                     'companies',
                                     'areas'
                                     ])->where('employees_id', $employee->id)->get();
-
         return view ('admin.pages.employee.show',[
             'employee'                  => $employee,
             'golongans'                 => $golongans,
@@ -200,7 +190,6 @@ class EmployeeController extends Controller
         $positions      = Positions::all();
         $working_hours  = WorkingHours::all();
         $areas          = Areas::all();
-
         return view ('admin.pages.employee.edit',[
             'employee'      => $employee,
             'golongans'     => $golongans,
@@ -227,7 +216,6 @@ class EmployeeController extends Controller
         $fileFields = [
             'foto_karyawan' => 'assets/foto/karyawan'
         ];
-
         foreach ($fileFields as $field => $path) {
             if ($request->hasFile($field)) {
                 // Ambil file
@@ -252,9 +240,7 @@ class EmployeeController extends Controller
                 unset($data[$field]);
             }
         }
-
         $employee->update($data);
-
         Alert::success('Success Update Data Karyawan','Oleh '.auth()->user()->name);
         return redirect()->route('employee.index');
     }
@@ -271,22 +257,18 @@ class EmployeeController extends Controller
 
         DB::transaction(function () use ($id) {
             $employee = Employees::findOrFail($id);
-
             $employee->update([
                 'hapus_oleh' => auth()->user()->name
             ]);
-
             $fileFields = [
                 'foto_karyawan' => 'assets/foto/karyawan'
             ];
-
             // Proses penghapusan file fisik dari storage
             foreach ($fileFields as $field => $path) {
                 if ($employee->$field) {
                     Storage::disk('public')->delete($path . '/' . $employee->$field);
                 }
             }
-
             $employee->delete();
         });
 
@@ -360,7 +342,6 @@ class EmployeeController extends Controller
         $this->fpdf->setLeftMargin(2);
         $this->fpdf->SetAutoPageBreak(false);
         $this->fpdf->AddPage();
-
         $this->fpdf->Cell(205, 293, '', 0, 0, 'C');
         $this->fpdf->SetFont('Arial', 'B', '8');
         $this->fpdf->Ln(30);
@@ -372,78 +353,61 @@ class EmployeeController extends Controller
         $this->fpdf->Cell(3);
         $this->fpdf->Cell(200, 10, 'No.' . $nomor . '/HRD/PK/'. $romawi . '/'.$tahun.'', 0, 0, 'C');
         $this->fpdf->Ln(30);
-
         $this->fpdf->SetFont('Arial', '', '12');
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(100, 10, 'Kami Yang Bertanda Tangan Dibawah Ini :', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(50, 10, 'Nama', 0, 0, 'L');
         $this->fpdf->Cell(100, 10, ' : Achmad Firmansyah', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(50, 10, 'Jabatan', 0, 0, 'L');
         $this->fpdf->Cell(100, 10, ' : Manager (HRD-GA)', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(50, 10, 'Menerangkan Bahwa', 0, 0, 'L');
         $this->fpdf->Cell(100, 10, '  ', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(50, 10, 'Nama', 0, 0, 'L');
         $this->fpdf->Cell(100, 10, ' : ' . $item->nama_karyawan . '', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(50, 10, 'NIK ', 0, 0, 'L');
         $this->fpdf->Cell(100, 10, ' : ' . $item->nik_karyawan. '', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(50, 10, 'Jabatan / Penempatan ', 0, 0, 'L');
         $this->fpdf->Cell(100, 10, ' : ' . $item->positions->jabatan . ' / ' . $item->divisions->penempatan . '', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(50, 10, 'Tanggal Mulai Kerja', 0, 0, 'L');
         $this->fpdf->Cell(100, 10, ' : ' . \Carbon\Carbon::parse($item->tanggal_mulai_kerja)->isoformat('D MMMM Y') . '', 0, 0, 'L');
         $this->fpdf->Ln(9);
-
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(180, 10, 'Adalah benar yang bersangkutan masih bekerja dan aktif menjadi karyawan kami,', 0, 0, 'L');
         $this->fpdf->Ln(5);
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(180, 10, 'dengan jabatan dan masa kerja di atas.', 0, 0, 'L');
-
         $this->fpdf->Ln(5);
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(180, 10, 'Demikianlah surat keterangan ini kami buat untuk digunakan dengan seperlunya.', 0, 0, 'L');
-
         $this->fpdf->Ln(10);
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(180, 10, 'Tangerang Selatan, ' . \Carbon\Carbon::now()->isoformat('D MMMM Y') . '', 0, 0, 'L');
-
         $this->fpdf->Ln(5);
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(180, 10, 'Hormat kami,', 0, 0, 'L');
-
         $this->fpdf->Ln(38);
-
         $this->fpdf->SetFont('Arial', 'BU', '12');
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(180, 10, 'Achmad Firmansyah', 0, 0, 'L');
-
         $this->fpdf->Ln(5);
         $this->fpdf->SetFont('Arial', 'B', '12');
         $this->fpdf->Cell(15);
         $this->fpdf->Cell(180, 10, 'Manager (HRD-GA)', 0, 0, 'L');
-
         $this->fpdf->Output();
-
         exit;
     }
 
@@ -637,19 +601,15 @@ class EmployeeController extends Controller
         // Auto width
         $highestColumn = $sheet->getHighestColumn(); 
         $highestColumnIndex = Coordinate::columnIndexFromString($highestColumn);
-
         for ($col = 1; $col <= $highestColumnIndex; $col++) {
             $columnLetter = Coordinate::stringFromColumnIndex($col);
             $sheet->getColumnDimension($columnLetter)->setAutoSize(true);
         }
         $writer = new Xlsx($spreadsheet);
-
         $filename = 'DatabaseKaryawan.xlsx';
-
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment;filename="'.$filename.'"');
         header('Cache-Control: max-age=0');
-
         $writer->save('php://output');
         exit;
     }
